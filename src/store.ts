b30429a992037aec025d8bd9c71cbed884f85f4d
Bryce Jech <bryce@brycejech.com>
2019-10-05 12:05:05 -0500

    add client ref to store

--- src/store.ts
+++ src/store.ts
@@ -3,14 +3,21 @@
 import Vue  from 'vue';
 import Vuex from 'vuex';
 
+import { IClient } from '@/interfaces';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-
+        client: <IClient|null>null
+    },
+    getters: {
+        client(state){ return state.client }
     },
     mutations: {
-
+        setClient(state, client: IClient){
+            state.client = client;
+        }
     },
     actions: {
 
