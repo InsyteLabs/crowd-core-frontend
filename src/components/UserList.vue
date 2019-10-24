e66324cd526e7219c333e617760b33818860f746
Bryce Jech <bryce@brycejech.com>
2019-10-23 19:07:20 -0500

    add delete user functionality

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -9,6 +9,7 @@
                 <th>Roles</th>
                 <th>Disabled</th>
                 <th>Disabled Comment</th>
+                <th class="text-center">Manage</th>
             </thead>
             <tbody v-if="users">
                 <tr v-for="user of users" :key="user.id">
@@ -25,6 +26,13 @@
                         }}
                     </td>
                     <td>{{ user.disabledComment }}</td>
+                    <td class="text-center">
+                        <button @click="deleteUser(user)" class="btn btn-sm btn-danger">
+                            <svg class="btn-icon delete" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path fill="currentColor" d="M296 432h16a8 8 0 0 0 8-8V152a8 8 0 0 0-8-8h-16a8 8 0 0 0-8 8v272a8 8 0 0 0 8 8zm-160 0h16a8 8 0 0 0 8-8V152a8 8 0 0 0-8-8h-16a8 8 0 0 0-8 8v272a8 8 0 0 0 8 8zM440 64H336l-33.6-44.8A48 48 0 0 0 264 0h-80a48 48 0 0 0-38.4 19.2L112 64H8a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h24v368a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V96h24a8 8 0 0 0 8-8V72a8 8 0 0 0-8-8zM171.2 38.4A16.1 16.1 0 0 1 184 32h80a16.1 16.1 0 0 1 12.8 6.4L296 64H152zM384 464a16 16 0 0 1-16 16H80a16 16 0 0 1-16-16V96h320zm-168-32h16a8 8 0 0 0 8-8V152a8 8 0 0 0-8-8h-16a8 8 0 0 0-8 8v272a8 8 0 0 0 8 8z"></path>
+                            </svg>
+                        </button>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -34,12 +42,15 @@
 <script lang="ts">
 'use strict';
 
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Vue, Component, Prop, Emit } from 'vue-property-decorator';
 import { User } from '@/models';
 
 @Component
 export default class UserList extends Vue {
     @Prop() users!: User[];
+
+    @Emit('deleteUser')
+    deleteUser(user: User): void{ }
 
 }
 </script>
